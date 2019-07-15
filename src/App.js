4b70178550f2f3b9f3dffdc2aaec47bd4b8cbc6b
andrijaDomazet <domazet.andrija@gmail.com>
2019-07-15 17:59:32 +0200

    Pokretanje aplikacije

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p style={{ fontSize: "50px" }}>Partibrejkers</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
